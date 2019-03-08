9aac2d773278ccf3941d1ed993268fd56d9f63f6
Everton Barros Jr <ebarrosjr@gmail.com>
2019-03-08 09:44:42 -0300

    Excluindo vulnerabilidade

--- webroot/sing-app-master/src/helpers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  activeClass: function(currentPartialName, partialNamesStr, options) {
-    return partialNamesStr.split(' ').indexOf(currentPartialName) !== -1 ? options.fn(this) : '';
-  },
-  
-  // Other helpers goes here
-};
